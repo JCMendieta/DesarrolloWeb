0f8b4b6f9248f6cbe50b3bda4896d108a18e02c7
Noonquiver <camilo.hernandez25@hotmail.com>
2022-02-25 22:45:48 -0500

    Base de datos no fake, repositorios para cada clase y buena parte de las relaciones

--- proyectoWeb/src/main/java/co/edu/javeriana/proyectoWeb/ProyectoWebApplication.java
+++ proyectoWeb/src/main/java/co/edu/javeriana/proyectoWeb/ProyectoWebApplication.java
@@ -2,13 +2,8 @@ package co.edu.javeriana.proyectoWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.xml.SourceHttpMessageConverter;
-
-import co.edu.javeriana.proyectoWeb.baseDeDatos.DB;
 
 //@SpringBootApplication
 @Configuration
@@ -18,10 +13,5 @@ public class ProyectoWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoWebApplication.class, args);
-	}
-
-	@Bean
-	public DB createDB(){
-		return new DB();
 	}
 }
